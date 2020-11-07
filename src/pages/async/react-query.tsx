ea4ab24a42329d66e014680ba714c4e0f859beec
Joakim Gunst <joakim.gunst@nitor.com>
2020-11-07 07:31:01 +0200

    Add react query example

---
+++ src/pages/async/react-query.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import AddForm from "components/AddForm";
+import TodoList from "components/TodoList";
+import { Todos } from "model";
+import * as api from "api";
+import TodoLayout from "components/TodoLayout";
+import LoadingSpinner from "components/LoadingSpinner";
+import {
+  useMutation,
+  useQuery,
+  useQueryCache,
+  QueryCache,
+  ReactQueryCacheProvider,
+} from "react-query";
+
+const queryCache = new QueryCache();
+
+export default function Page() {
+  return (
+    <ReactQueryCacheProvider queryCache={queryCache}>
+      <TodoLayout title="SWR">
+        <AddFormContainer />
+        <TodoListContainer />
+      </TodoLayout>
+    </ReactQueryCacheProvider>
+  );
+}
+
+function AddFormContainer() {
+  const queryCache = useQueryCache();
+
+  const [addTodo] = useMutation(api.addTodo, {
+    onSuccess: () => queryCache.invalidateQueries("todos"),
+  });
+
+  return <AddForm onAdd={addTodo} />;
+}
+
+function TodoListContainer() {
+  const queryCache = useQueryCache();
+  const { data: todos } = useQuery<Todos>("todos", api.getTodos);
+
+  const [deleteTodo] = useMutation(api.deleteTodo, {
+    onSuccess: () => queryCache.invalidateQueries("todos"),
+  });
+
+  if (!todos) return <LoadingSpinner />;
+  return <TodoList todos={todos} onDelete={todo => deleteTodo(todo.id)} />;
+}
